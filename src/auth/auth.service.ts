41b9d5d8f27e26186f4031c1c56c74845bf6b3c1
Tatiana Kashcha <tkashcha@gmail.com>
2025-05-31 23:00:27 +0300

    add strategy jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
+  // для стратегії "local"
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
     if (user && user.password === pass) {
@@ -12,5 +17,13 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  // для стратегії "jwt"
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
